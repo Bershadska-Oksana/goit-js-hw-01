b42c9bae9c130adf5d915f2c7b9cc862049cd929
Oksana Bershadska <ksenaua@icloud.com>
2025-06-04 19:47:06 +0200

    update

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ function getElementWidth(content, padding, border) {
   return (
     Number.parseFloat(content) +
     Number.parseFloat(padding) * 2 +
-    Number.parseFloat(border)
+    Number.parseFloat(border) * 2
   );
 }
 
